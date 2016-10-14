1c4bd4702eac906be72c916b013f40edb25b71f4
Ilana <ilanaenderman@hotmail.com>
2016-10-14 12:22:12 +0200

    app done

--- app.js
+++ app.js
@@ -2,6 +2,19 @@
 // importing necessary modules
 const fs = require('fs')  //you can reed const as var
 
+// Helper functions
+var roundDecimal = ( number ) => {
+	return Math.round(number * 100) / 100
+}
+
+var addCommas = ( number ) => {
+	return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+}
+
+var prettyNumber = (number) =>{
+	return addCommas(roundDecimal(number))
+}
+
 // read the costumer data json
 //=> is same as writing functions
 
@@ -21,23 +34,24 @@ var calcCompound = ( customer  ) => {
 
 	//Set end amount prop and  and calculate total duration
 	customer.pension.endamount = {
-		pessimistic: customer.finances.startcapital,
-		average: customer.finances.startcapital,
-		optimistic: customer.finances.startcapital
+	pessimistic: customer.finances.startcapital,
+	average: 	 customer.finances.startcapital,
+	optimistic:  customer.finances.startcapital
 	}	
 	customer.pension.duration = (customer.pension.age - customer.age)
 
 	//Do the interest math
 	for (var i = customer.pension.duration - 1; i >= 0; i--) {
-		//add monthly spend to all the scenario's
-		customer.pension.endamount.pessimistic += ( customer.finances.monthlyadd * 12 )
-		customer.pension.endamount.average += ( customer.finances.monthlyadd * 12 )
-		customer.pension.endamount.optimistic += ( customer.finances.monthlyadd * 12 )
 
-		//calculate the added interest
-		customer.pension.endamount.pessimistic *= customer.pension.interest.pessimistic
-		customer.pension.endamount.average *= customer.pension.interest.average
-		customer.pension.endamount.optimistic *= customer.pension.interest.optimistic
+	//add monthly spend to all the scenario's
+	customer.pension.endamount.pessimistic 	+= ( customer.finances.monthlyadd * 12 )
+	customer.pension.endamount.average 		+= ( customer.finances.monthlyadd * 12 )
+	customer.pension.endamount.optimistic 	+= ( customer.finances.monthlyadd * 12 )
+
+	//calculate the added interest
+	customer.pension.endamount.pessimistic 	*= 	customer.pension.interest.pessimistic
+	customer.pension.endamount.average 		*= 	customer.pension.interest.average
+	customer.pension.endamount.optimistic 	*= 	customer.pension.interest.optimistic
 
 	}
 
@@ -47,7 +61,7 @@ var calcCompound = ( customer  ) => {
 	console.log( "When you retire ate age " + customer.pension.age + " you will have the following:")
 
 	//output calculation stuf
-	console.log( "In a pessimistic scenario: $" + customer.pension.endamount.pessimistic)
-	console.log( "In an average scenario: $" + customer.pension.endamount.average)
-	console.log( "In a optimistic scenario: $" + customer.pension.endamount.optimistic)
-}
+	console.log( "In a pessimistic scenario: €" + 	prettyNumber(customer.pension.endamount.pessimistic) )
+	console.log( "In an average scenario: €" 	+ 	prettyNumber(customer.pension.endamount.average) )
+	console.log( "In a optimistic scenario: €" 	+ 	prettyNumber(customer.pension.endamount.optimistic) )
+	}
